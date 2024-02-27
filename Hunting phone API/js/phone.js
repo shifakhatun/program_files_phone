34e3936ea445c02fd01f1877f70b1c232a0d9e4b
shifa <shifa.eee@gmail.com>
2024-02-28 00:18:36 +0600

    first commit

---
+++ Hunting phone API/js/phone.js
@@ -0,0 +1,18 @@
+//console.log('hunting phone')
+
+const loadPhone= async()=>{
+    const res= await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+
+    const data= await res.json();
+    const phones=data.data;
+    // console.log(phones);
+    displayPhones(phones)
+}
+const displayPhones=phones=>{
+    console.log(phones);
+    phones.forEach(phone => {
+        console.log(phone)
+    });
+
+}
+loadPhone();
